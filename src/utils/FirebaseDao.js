85deb635e3ac429dd96c43796f7b7081c87d40a5
yunchae <ck2204@naver.com>
2018-03-20 14:32:13 +0900

    - 외부 변수 -> this 자신 객체를 가리키도록 수정

--- src/utils/FirebaseDao.js
+++ src/utils/FirebaseDao.js
@@ -4,21 +4,21 @@ import config from '../assets/config'
 firebase.initializeApp(config.FIREBASE_CONFIG);
 
 // Get a reference to the database service
-var database;
 
 export default class FirebaseDao {
+
   constructor() {
-    database = firebase.database();
+    this.database = firebase.database();
   }
 
   readBooks(callback) {
-    database.ref('books/').once('value').then(function(snapshot){
+    this.database.ref('books/').once('value').then(function(snapshot){
       callback(snapshot.val());
     })
   }
 
   insertBook(book) {
-    database.ref('books/' + book.isbn).set({
+    this.database.ref('books/' + book.isbn).set({
       title : book.title,
       author : book.author,
       publishedDate : book.publishedDate,
@@ -30,7 +30,7 @@ export default class FirebaseDao {
   }
 
   updateBook(book){
-    database.ref('books/' + book.isbn).update({
+    this.database.ref('books/' + book.isbn).update({
       title : book.title,
       author : book.author,
       publishedDate : book.publishedDate,
