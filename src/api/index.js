6159e1d7d6aa5afafc52dd9c71de56e423582de1
yunchae.na <ck2204@naver.com>
2018-03-28 10:01:55 +0900

    1. api 모듈 리팩토링 2. 호출 url 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,11 @@
 import axios from 'axios'
 
-const instance = axios.create({
-  timeout: 5000
-  ,
-  headers: {
-    'X-Naver-Client-Id': '3hWvxsSHqpPmvgYQ_u6N',
-    'X-Naver-Client-Secret': 'oxvuFisDg5'
-  }
-});
+
 
 axios.defaults.timeout = 5000;
 axios.defaults.headers.get['Content-Type'] = 'application/json';
+
+const instance = axios.create();
 
 instance.interceptors.request.use(config => {
   return config;
@@ -25,15 +20,7 @@ instance.interceptors.response.use(response => {
 })
 
 export default {
-  // generatePFS: function (param) {
-  //   return instance.post(`${resources.URI}/v1/peerfeedback`, param);
-  // }
-  // login: function (feedbackToken, adminToken, password) {
-  //   return instance.get(`${resources.URI}/v1/peerfeedback/${feedbackToken}/${adminToken}?logininfo={"pw":"${password}"}`);
-  // }
-
   searchBook: function(title){
-
-    return instance.get(`http://13.125.241.38:8080/books?title=${title}`)
+    return instance.get(`https://act-books-api.zz.am/books?title=${title}`)
   }
 }
