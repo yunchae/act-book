407b113bbbc3ee90d0a17bd937df4da78f0d70d1
yunchae.na <ck2204@naver.com>
2018-03-29 15:48:03 +0900

    api.searchBook 테스트

--- test/unit/specs/api/index.spec.js
+++ test/unit/specs/api/index.spec.js
@@ -4,19 +4,14 @@ import axios from 'axios'
 
 describe('Test Case Api call', ()=>{
 
-  beforeEach(()=> {
-    sinon.spy(axios, 'get');
-  })
-
   it('api.js 에서 get 을 제대로 호출하는지 테스트', async()=>{
 
+    sinon.spy(axios, 'get');
     api.searchBook('java');
-    expect(axios.get.withArgs('https://act-books-api.zz.am/books?title=java').calledOnce);
 
-  })
-  afterEach(() => {
+    expect(axios.get.withArgs('https://act-books-api.zz.am/books?title=java').calledOnce).toBe(true);
+
     axios.get.restore();
   })
-
 })
 
