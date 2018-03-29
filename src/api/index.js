142ce6dbc86b8e8577fec2e5fa1c8c4f62855e76
yunchae.na <ck2204@naver.com>
2018-03-29 15:45:57 +0900

    api -> axios 객체 생성 삭제

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,10 @@
 import axios from 'axios'
 
-
-
 axios.defaults.timeout = 5000;
 axios.defaults.headers.get['Content-Type'] = 'application/json';
 
-const instance = axios.create();
-
-instance.interceptors.request.use(config => {
-  return config;
-}, err => {
-  return Promise.reject(err);
-})
-
-instance.interceptors.response.use(response => {
-  return response
-}, err => {
-  return Promise.reject(err)
-})
-
 export default {
   searchBook: function(title){
-    return instance.get(`https://act-books-api.zz.am/books?title=${title}`)
+    return axios.get(`https://act-books-api.zz.am/books?title=${title}`)
   }
 }
