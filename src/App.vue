812b36a9f74bea63d9414e57edc2b43809e2cad9
yunchae.na <ck2204@naver.com>
2018-04-02 20:15:12 +0900

    1.페이지 제목 대문자로 변경 2.로고 추가

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand">Act-Book</a>
+            <a class="navbar-brand">ACT-BOOK</a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
